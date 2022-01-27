c014c1bc73194fd5a221838a3e7d6b91883c88e5
lslbastos <lslbastos@gmail.com>
2022-01-27 08:57:13 -0300

    Manual update after adding URL for 2022 data 2022-01-27

--- code/App Data Preparation Functions/app_update_sivep.R
+++ code/App Data Preparation Functions/app_update_sivep.R
@@ -111,7 +111,7 @@ check_last_update <- function(release_date) {
 library(tidyverse)
 
 ## gets current date
-current_date <- as.Date(Sys.time()) # data is from 2 days before the release date
+current_date <- as.Date(Sys.time()) - 1 # data is from 2 days before the release date
 # current_date <- as.Date("2022-01-10") # data is from 2 days before the release date
 # current_date <- "2021-07-19"
 
